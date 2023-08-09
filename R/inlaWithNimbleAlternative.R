ec25b27720eb2e807d01824dc38b40fc5280d61f
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2023-08-09 20:24:31 +0200

    updates

--- R/inlaWithNimbleAlternative.R
+++ R/inlaWithNimbleAlternative.R
@@ -39,51 +39,52 @@ INLAWiNimDataGeneratingTargetDivide <- function(data,
   fixedVals <- parametersToMonitor$inla
   target <- parametersToMonitor$mcmc
   targetMCMC <- parametersToMonitor$mcmc2
-  
+
   #set up initial value
-  
+
   initsList <- modelInits()
-  
+
   #Create the model in nimble
   mwtc <- nimble::nimbleModel(code,
                               data = modelData,
                               constants = modelConstants,
                               inits = initsList)
-  
+
   # Create the model in C
   Cmwtc <- nimble::compileNimble(mwtc,
                                  showCompilerOutput = FALSE) #Have issues compiling
-  
+
   #create list to return
   retList <- list()
   # Fit INLAMCMC
-  
+
   if(inlaMCMC %in% "inlamcmc"){
     mcmcconf <- nimble::configureMCMC(Cmwtc,
                                       nodes = c(target))
-    
+
     # setting sampler controls
     samplerControl$fit.inla = nimbleINLA
     samplerControl$x = x
     samplerControl$y = y
     samplerControl$fixedVals = fixedVals
     samplerControl$fam = family
-    
+    samplerControl$targetMCMC = targetMCMC
+
     # mcmc configuration
     mcmcconf$addSampler(target = targetMCMC,
                         type = inlaMCsampler,
                         control = samplerControl)
-    
+
     mcmcconf$printSamplers(executionOrder = TRUE)
     mcmcconf$addMonitors(c(target, targetMCMC))
-    
+
     if(!is.null(parametersToMonitor$additionalPars)){
       mcmcconf$addMonitors(parametersToMonitor$additionalPars)
     }
-    
+
     #build model
     Rmcmc <- nimble::buildMCMC(mcmcconf)
-    
+
     # Compile
     cmcmc <- nimble::compileNimble(Rmcmc,
                                    project = Cmwtc,
@@ -103,23 +104,23 @@ INLAWiNimDataGeneratingTargetDivide <- function(data,
     endTime <- Sys.time()
     timeTaken <- difftime(endTime, startTime, units = "secs")
     #as.numeric(endTime - startTime)
-    
+
     ret <- list(mcmc.out = mcmc.out,
                 timeTaken = timeTaken)
-    
+
     #save inlamcmc results
     retList$inlamcmc <- ret
   }
-  
+
   if(inlaMCMC %in% "mcmc"){
     mcmcconf <- nimble::configureMCMC(Cmwtc,
                                       monitors = c(target, fixedVals, targetMCMC))
-    
+
     mcmcconf$printSamplers()
-    
+
     #build model
     Rmcmc <- nimble::buildMCMC(mcmcconf)
-    
+
     # Compile
     cmcmc <- nimble::compileNimble(Rmcmc,
                                    project = Cmwtc,
@@ -138,10 +139,10 @@ INLAWiNimDataGeneratingTargetDivide <- function(data,
                                 WAIC = mcmcConfiguration[["WAIC"]])
     endTime <- Sys.time()
     timeTaken <- difftime(endTime, startTime, units = "secs")
-    
+
     ret <- list(mcmc.out = mcmc.out,
                 timeTaken = timeTaken)
-  
+
     #save results for MCMC
     retList$mcmc <-  ret
   }
