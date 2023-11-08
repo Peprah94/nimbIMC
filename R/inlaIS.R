defcac121912f9711c16e43d1fdecdb06107b932
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2023-11-08 09:54:13 +0100

    updates

--- R/inlaIS.R
+++ R/inlaIS.R
@@ -45,6 +45,7 @@ impSampINLAstep <- nimbleFunction(
   contains = importanceSamplingStepVirtual,
   setup = function(model,
                    mvEWSamples,
+                   mvWSamples,
                    fixedVals,
                    iNode,
                    x, #covariates
@@ -75,6 +76,11 @@ impSampINLAstep <- nimbleFunction(
 
     indInc <- (iNode - 1)*timeIndex
     m <- timeIndex
+    betaEsts <- matrix(0, nrow = m, ncol = nBetaSims)
+    betaEstsUpd <- matrix(0, nrow = m+indInc, ncol = (nBetaSims+1))
+    rts <- matrix(0, nrow = m+indInc, ncol = nBetaSims)
+    rtsUpd <- matrix(0, nrow = m+indInc, ncol = nBetaSims)
+    muEsts <- matrix(0, nrow = m+indInc, ncol = nBetaSims)
     #cummN <- timeIndex
     #T <- length(cummN)
     #No resampling will be done here
@@ -85,19 +91,19 @@ impSampINLAstep <- nimbleFunction(
                  #t = integer(0),
                  prevSamp= integer(0)) {
     returnType(double(0))
-
+#print(1)
     wts <- numeric(m, init=FALSE)
     gamma <- numeric(m, init=FALSE)
     nugs <- numeric(nNugget, init = FALSE)
-    betaEsts <- matrix(0, nrow = m, ncol = nBetaSims)
-    #ids <- integer(m, 0)
-   # llEst <- numeric(m, init=FALSE)
 
-    #llEst <- res[1,1]
-#print(sigmaBeta)
-#Length of previous iterations
-#prevIter <- sum(cummN[1:T]) - Nt
-
+#copy results from mvEWSamples to mvWSamples
+    # for(i in 1:m){
+    #   nimCopy(mvEWSamples, mvWSamples, nodes = "gamma", row = i)
+    #   nimCopy(mvEWSamples, mvWSamples, nodes = "logLike", row = i)
+    #   nimCopy(mvEWSamples, mvWSamples, nodes = "wts", row = i)
+    # }
+   #mvWSamples <<-  mvEWSamples
+#print(2)
 Nt <- m # Number of samples at Nt
     for(i in 1:m){
       # For now, simulate beta's from proposal distribution
@@ -113,9 +119,10 @@ Nt <- m # Number of samples at Nt
       }
         #}
      # for(i in 1:m){
-        betaEsts[i, 1:nBetaSims] <- betaVals#mvEWSamples[beta, (i+indInc)] #nimbleconvertToMatrix(mvEWSamples, beta)
+        betaEsts[i, 1:nBetaSims] <<- betaVals#mvEWSamples[beta, (i+indInc)] #nimbleconvertToMatrix(mvEWSamples, beta)
 
     }
+#print(3)
       res <- nimbleINLA(x, y, beta= betaEsts, fixedVals,  family = fam, nCores = nCores)
 
       #Calulate weights
@@ -128,49 +135,112 @@ Nt <- m # Number of samples at Nt
       for(i in 1:m){
         betaVals <<- betaEsts[i, 1:nBetaSims]
       for(j in 1:iNode){
-        nugs[j] <- j * timeIndex * dmnorm_chol(betaVals, meanBeta, chol(sigmaBeta), prec_param = FALSE)
+        nugs[j] <- timeIndex * dmnorm_chol(betaVals, meanBeta, chol(sigmaBeta), prec_param = FALSE)
       }
         gamma[i] <- sum(nugs[1:iNode])
-
+#print(4)
      # }
       #print(gamma)
       values(model, beta) <<- betaVals
-      wts[i] <- (res[i,1]*model$calculate(beta))/(gamma[i]/(iNode*timeIndex))
-#print(wts)
-#k <- i + indInc #Increases with m and Nt
-#print(k)
-      if(prevSamp == 1){
-        mvEWSamples["gamma",i][iNode] <<- gamma[i] + (m * dmnorm_chol(betaVals, meanBeta, chol(sigmaBeta), prec_param = FALSE))
-        mvEWSamples["wts",i][iNode] <<- (res[i,1]*model$calculate(beta))/(mvEWSamples["gamma",i ][iNode]/(timeIndex * iNode))
-      }else if(prevSamp == 0){
-        mvEWSamples["gamma",i][iNode] <<- gamma[i]
-        mvEWSamples["wts", i][iNode] <<- wts[i]
-        }
-      #save results
+      loglike <- res[i,1]*model$calculate(beta)
+      wts[i] <- (loglike)/(gamma[i]/(iNode*timeIndex))
+      #print(99)
+      mvWSamples["logLike",i][iNode] <<- loglike
+      #print(999)
+      mvEWSamples["logLike",i][iNode] <<- loglike
+#print(5)
+      # save results for current time
       saveResults(fixedVals, res, ind = i)
-
-      #for(k in 1:length(vars)){
-        #rt <- vars[2]
-     # mvEWSamples[rt, i][,iNode] <<-  values(model,rt)
-      #}
       k <- i + indInc
-     # print(k)
-     nimCopy(model, mvEWSamples, nodes = beta, nodesTo = beta, row = k, rowTo = k)
-     nimCopy(model, mvEWSamples, nodes = fixedVals, nodesTo = fixedVals, row = k, rowTo =k)
+      nimCopy(model, mvEWSamples, nodes = beta, nodesTo = beta, row = k)
+      nimCopy(model, mvEWSamples, nodes = fixedVals, nodesTo = fixedVals, row = k)
+#print(6)
+      mvEWSamples["gamma",i][iNode] <<- gamma[i]
+      mvEWSamples["wts", i][iNode] <<- wts[i]
+#print(7)
+
+#Saving output for updated mean and sd
+betaEstsUpd[k,1:nBetaSims] <<- betaVals
+betaEstsUpd[k,(nBetaSims+1)] <<- wts[i]
+
+    if(iNode > 1){
+        for(t in 1:(iNode-1)){
+          indx <- i + (t-1)*m
+          #print(i)
+         # print(t)
+          nimCopy(mvEWSamples, model, nodes = beta, nodesTo = beta, row = indx)
+          betaValsNew <- values(model, beta)
+          if(prevSamp == 1){
+            if(proposal == "normal"){
+              priorDist <- dmnorm_chol(betaValsNew, meanBeta, chol(sigmaBeta), prec_param = FALSE)
+            }
+            if(proposal == "studentT"){
+              priorDist <- dmvt_chol(betaValsNew, mu = meanBeta, chol(sigmaBeta), df= dfTdist,prec_param = FALSE)
+            }
+            if(proposal == "prior"){
+              priorDist <- model$calculate(beta)
+              #model$simulate(nodes = betaNames)
+              #betaVals <<- values(model, betaNames)
+            }
+        #priorDist <- dmnorm_chol(betaValsNew, meanBeta, chol(sigmaBeta), prec_param = FALSE)
+        ret <-  mvEWSamples["gamma",i][t] #trying to trick nimble
+        gammaUpd <- ret + (m * priorDist)
+        mvEWSamples["gamma",i][t] <<-  gammaUpd
+        mvEWSamples["wts",i][t] <<- (mvEWSamples["logLike",i][t])/(gammaUpd/(m * iNode))
+        print(indx)
+         }
+          betaEstsUpd[indx,1:nBetaSims] <<- betaValsNew
+          betaEstsUpd[indx,(nBetaSims+1)] <<- mvEWSamples["wts",i][t]
+        }
+
+
+      #save results
+
       #calculate
       #wts[i]  <- model$calculate(calc_thisNode_deps)
-    }
-
-    #Normalised weights
+    }#else{
+     # nimCopy(mvEWSamples, model, nodes = beta, nodesTo = beta, row = i)
+     # betaValsNew <- values(model, beta)
+     # betaEstsUpd[i,1:nBetaSims] <<- betaValsNew
+     # betaEstsUpd[i,(nBetaSims+1)] <<- wts[i]
+    #}
+}
+    #Normalised weights for Effective Sample size
     nWeights <- wts/sum(wts)
-    mu <<- (nWeights %*% betaEsts)[1, 1:nBetaSims]
-    for(i in 1:m){
-    sigma <<- sigma + nWeights[i] * (t(t(betaEsts[i, 1:nBetaSims] - mu[1:nBetaSims]))%*%((betaEsts[i, 1:nBetaSims] - mu[1:nBetaSims])))
+    ess <<- 1/sum(nWeights^2)
+
+
+    # mu and sigma updates
+    K <- m + indInc
+    #print(K)
+    nWeightsUpd <- betaEstsUpd[1:K,(nBetaSims+1)]/sum(betaEstsUpd[1:K,(nBetaSims+1)])
+    #print(nWeightsUpd)
+    for(j in 1:nBetaSims){
+    for(i in 1:K){
+      muEsts[i, j] <<- nWeightsUpd[i] * betaEstsUpd[i, j]
     }
+      mu[j] <<- sum(muEsts[1:K, j])
+    }
+    #for(i in 1:nBetaSims){
+   ## mu[i] <<- #(t(nWeightsUpd[1:K]) %*% betaEstsUpd[1:K,1:nBetaSims])[1, 1:nBetaSims]
+#}
+#print(10000)
+for(j in 1:nBetaSims){
+     for(i in 1:K){
+       #rr <- nWeightsUpd[i] * (betaEstsUpd[i, 1:nBetaSims] - mu[1:nBetaSims])
+    rts[i, j] <<-  nWeightsUpd[i] * (betaEstsUpd[i, j] - mu[j])
+    rtsUpd[i,j] <<- (betaEstsUpd[i, j] - mu[j])
+     }
+}
+    sigma <<-  t(rts)%*%rtsUpd
+      #(t(rts[1:K, 1:nBetaSims]))%*%((betaEstsUpd[1:K, 1:nBetaSims] - mu[1:nBetaSims]))
+
+    print(mu)
+    print(sigma)
     #save the values
 
 lll <- res[1,1]
-ess <<- 1/sum(nWeights^2)
+
     # if(lll == -Inf){
     #   copy(mvEWSamples, model, nodes = fixedVals, row = 1)
     # }else{
@@ -282,19 +352,25 @@ inlaIS <- nimbleFunction(
 
     #size$beta <- 4
     # Add names and dimensions for wts and gamma
-    names <- c(names, "wts", "gamma")
-    type <- c(type, "double", "double")
-    size$wts <- timeIndex
-    size$gamma <- timeIndex
-
+    names <- c(names, "wts", "gamma","logLike")
+    type <- c(type, "double", "double", "double")
+    size$wts <- nSteps
+    size$gamma <- nSteps
+    size$logLike <- nSteps
+#print(1)
     #model values to save results
     mvEWSamples <- modelValues(modelValuesConf(vars = names,
+                                               types = type,
+                                               sizes = size))
+
+    mvWSamples <- modelValues(modelValuesConf(vars = names,
                                                types = type,
                                                sizes = size))
 
     fixedVals <- model$expandNodeNames(fixedVals)
 
     multiple <- TRUE
+    #print(multiple)
     if(length(model$expandNodeNames(fixedVals)) == 1) multiple = FALSE
     # vals <- rep(0, length(fixedVals))
     #for(i in seq_along(fixedVals)){
@@ -307,6 +383,7 @@ inlaIS <- nimbleFunction(
     for(iNode in 1:nSteps){
     impSampINLAstepFnx[[iNode]] <- impSampINLAstep(model,
                                                 mvEWSamples,
+                                                mvWSamples,
                                                 fixedVals,
                                                 iNode,
                                                 x, #covariates
@@ -326,10 +403,11 @@ inlaIS <- nimbleFunction(
     meanBeta <- initMean; sigmaBeta <- initCov
     cummN <- timeIndex
     m <- timeIndex
+    #print(m)
    # T <- length(cummN)
     #M <- sum(cummN[1:T])
 #essVals <- rep(0, nSteps)
-prevSamp <- 0
+pp <- 0
     lastLogLik <- -Inf
   },
   run = function( ) {
@@ -338,6 +416,7 @@ prevSamp <- 0
     if(initModel) my_initializeModel$run()
 nIter <- nSteps*timeIndex
     resize(mvEWSamples, nIter)
+    resize(mvWSamples, nIter)
     essVals <- 0
 
     for(iNode in seq_along(impSampINLAstepFnx)){
@@ -348,11 +427,11 @@ nIter <- nSteps*timeIndex
     # meanBeta <<-   impSampINLAstepFnx[[t]]$updateBetaMean()
     # }else{
         #prevSamp = 1
-      essVals <- essVals + impSampINLAstepFnx[[iNode]]$run(meanBeta = meanBeta, sigmaBeta=sigmaBeta, prevSamp)
+      essVals <- essVals + impSampINLAstepFnx[[iNode]]$run(meanBeta = meanBeta, sigmaBeta=sigmaBeta, prevSamp = pp)
         meanBeta <<-   impSampINLAstepFnx[[iNode]]$updateBetaMean()
         sigmaBeta <<- impSampINLAstepFnx[[iNode]]$updateBetaSigma()
          #impSampINLAstepFnx[[iNode]]$returnESS()
-        prevSamp <<- 1
+        pp <<- 1
         #}
 }
     return(essVals)
